9dced3a33f99f962e2bd937a163ddeefebbcb9d3
Ludmila Mychta <ljuda12@gmail.com>
2021-01-10 16:38:05 +0300

    підключення до зовнішньої БД і запуск програми

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+
+async function start() {
+    console.log('Спроба підключення до Монго ДБ і запуск сервера')
+    try {
+        console.log('підключення до бази даних Монго ДБ')
+        await mongoose.connect(config.get('mongoUrl'), {
+            useNewUrlParser: true
+        })
+        app.listen(PORT, () => console.log(`Програма була запущена...`, PORT));
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+
+
+//const B = 9
+// A = B || 8;
+
 console.log('App start!');
